50823cfdbd92c790c6b7259befbed27d1a5ed459
MarianneBricquir <marianne.bricquir@gmail.com>
2022-11-21 13:44:41 +0100

    data map for a propos page

--- src/pages/APropos.js
+++ src/pages/APropos.js
@@ -1,6 +1,7 @@
 import Banner from '../components/Banner';
 import imageApropos from '../img/img-apropos.jpg';
 import Collapse from '../components/Collapse';
+import dataCollection from '../data/aproposData'
 
 const APropos = () => {
 
@@ -9,12 +10,11 @@ const APropos = () => {
             <Banner image={imageApropos} alt={"Vallée et montagnes sous le soleil"} bannerText={""} />
             <section className="apropos-section-2">
                 <div className="apropos-content">
-                    {<div className="accordions">
-                        <Collapse title="Fiabilité" content="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toute les informations sont régulièrement vérifiées par nos équipes" />
-                        <Collapse title="Respect" content="La bienveillance fait partie des valeurs fondatruces de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme" />
-                        <Collapse title="Service" content="Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question." />
-                        <Collapse title="Sécurité" content="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes." />
-                    </div>}
+                    {dataCollection.map((data, index) => {
+                        return (
+                            <Collapse key={index} title={data.title} content={data.content} />
+                        )
+                    })}
                 </div>
             </section >
         </div >
